f21eb8b1918b9e0388df5ec75717f1715504eb0e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-03 23:16:30 +0100

    Create static.c

---
+++ wykład_1/static.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int licznik()
+{
+  static int a;
+  a++;
+  return a;
+}
+
+int main()
+{
+  printf("Wywoluje funkcje licznik %d", licznik());
+  printf("\n...drugi raz %d", licznik());
+  printf("\n...trzeci raz %d", licznik());
+
+  return 0;
+}
+
+/*
+Wywoluje funkcje licznik 1
+...drugi raz 2
+...trzeci raz 3%   
+*/
