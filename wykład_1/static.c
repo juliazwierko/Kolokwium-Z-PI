84eed0e4582ccd563d89e77233993d4ba0ff59ff
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-05 08:48:18 +0100

    Update static.c

--- wykład_1/static.c
+++ wykład_1/static.c
@@ -14,12 +14,13 @@ int main()
   printf("Wywoluje funkcje licznik %d", licznik());
   printf("\n...drugi raz %d", licznik());
   printf("\n...trzeci raz %d", licznik());
-
+  
+  printf("\n");
   return 0;
 }
 
 /*
 Wywoluje funkcje licznik 1
 ...drugi raz 2
-...trzeci raz 3%   
+...trzeci raz 3  
 */
