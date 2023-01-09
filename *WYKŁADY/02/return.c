d33bfb83a263e6c77b8f8cf0987ade721ab3f3f6
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-09 18:13:10 +0100

    Update and rename tablice.c to return.c

---
+++ *WYKŁADY/02/return.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+
+int operacje (int a, int b)
+{
+    int suma = a + b;
+    return suma; //instrukcja skoku;
+}
+
+int main() {
+
+    int liczba1, liczba2;
+    printf("Wpisz 2 liczby:\n");
+    scanf("%d", &liczba1);
+    scanf("%d", &liczba2);
+
+    int dzialania = operacje(liczba1, liczba2);
+    printf("%d", dzialania);
+
+    return 0;
+}
