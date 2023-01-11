893dae03c4bb8b859d948c52582909e419d78f01
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-11 18:03:06 +0100

    Create zadanie_23.c

---
+++ Grażyna_kolos/zadanie_23.c
@@ -0,0 +1,12 @@
+//Zad 23. Dla podanych deklaracji: 
+int n=5, p=9, q; float x;
+//Jakie będą wartości wyrażeń oraz zmiennych n, p, q, x po każdej instrukcji 
+//(za każdym razem zaczynamy od takich samych wartości) - proszę spróbować przewidzieć, a potem wykonać 
+//program z dopisanymi printf’ami po każdej instrukcji (przed każą instrukcją resetowanie ustawień zmiennych)
+q = n<p
+q = n==p q=p%n+p>n
+x = p/n
+x = (float) p/n;
+x = (p+0.5)/n;
+x = (int)(p+0.5)/n
+q = n*(p>n ? n : p) q = n* (p<n ? n : p)
